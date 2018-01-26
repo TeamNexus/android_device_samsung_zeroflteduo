219332fb29eea8e9724fac3e53fe745f1ed69983
Lukas0610 <mail@lukasberger.at>
2018-01-26 22:23:23 +0100

    zeroflteduo: updated asserts to support Edge-variants

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 include device/samsung/zero-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := zeroflteduo,zerofltexx,zeroflte
+TARGET_OTA_ASSERT_DEVICE := zeroflteduo,zerofltexx,zeroflte,zerolteduo,zeroltexx,zerolte
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zeroflteduo/include
